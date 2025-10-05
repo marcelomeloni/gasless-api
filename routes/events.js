b2fbc28dc72039bc3d14f03a8136553a8d97fbbb
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-10-04 21:46:58 -0300

    Create events.js

---
+++ routes/events.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import multer from 'multer';
+import { createFullEvent, getActiveEvents, getEventDetails } from '../controllers/eventController.js';
+
+const router = express.Router();
+const upload = multer({ storage: multer.memoryStorage() });
+
+router.post(
+    '/create-full-event',
+    upload.fields([
+        { name: 'image', maxCount: 1 },
+        { name: 'organizerLogo', maxCount: 1 }
+    ]),
+    createFullEvent
+);
+
+router.get('/active', getActiveEvents);
+router.get('/:eventAddress', getEventDetails);
+
+export default router;

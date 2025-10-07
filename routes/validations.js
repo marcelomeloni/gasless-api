71cf87f1ed171a77fb6c5376e01e305657a3454a
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-10-06 22:27:19 -0300

    Update validations.js

--- routes/validations.js
+++ routes/validations.js
@@ -1,9 +1,11 @@
+// routes/validations.js
 import express from 'express';
-import { validateById, getValidatedTickets } from '../controllers/validationController.js';
+import { validateById, validateByIdWithFrontendSignature, getValidatedTickets } from '../controllers/validationController.js';
 
 const router = express.Router();
 
 router.post('/validate-by-id/:registrationId', validateById);
+router.post('/validate-by-id-frontend/:registrationId', validateByIdWithFrontendSignature);
 router.get('/event/:eventAddress/validated-tickets', getValidatedTickets);
 
 export default router;

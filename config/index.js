40da75ff19ded29c137db3e47bb7f290ea014bd9
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-10-08 18:47:14 -0300

    Update index.js

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ export const SUPABASE_URL = process.env.SUPABASE_URL;
 export const SUPABASE_SERVICE_KEY = process.env.SUPABASE_SERVICE_KEY;
 export const PINATA_JWT = process.env.PINATA_JWT;
 export const MERCADOPAGO_ACCESS_TOKEN = process.env.MERCADOPAGO_ACCESS_TOKEN;
-export const API_URL = process.env.API_URL || 'https://gasless-api-ke68.onrender.com';
+export const API_URL = process.env.API_URL || 'https://gasless-api-997m.onrender.com';
 export const FRONTEND_URL = process.env.FRONTEND_URL || 'http://localhost:5173';
 export const PROGRAM_ID = process.env.PROGRAM_ID;
 // Validations

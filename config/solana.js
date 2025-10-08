0866771c12376ebff190677eec6d8c5ce9a68414
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-10-08 14:47:18 -0300

    Update solana.js

--- config/solana.js
+++ config/solana.js
@@ -1,4 +1,4 @@
 import { PublicKey } from '@solana/web3.js';
 
-export const PROGRAM_ID = new PublicKey("5kQZsq3z1P9TQuR2tBXJjhKr46JnEcsDKYDnEfNCB792");
+export const PROGRAM_ID = new PublicKey("D5RUrHFK8c6W9tQsC2R1Z4BGkwbaNVkr8q74S8BqKXaT");
 export const TOKEN_METADATA_PROGRAM_ID = new PublicKey("metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s");
